31df471b7ed44c420293fc4eee8ca90d883f162d
RRangly <r.rangly@gmail.com>
2023-05-23 00:42:36 -0400

    CoinManager

--- src/ServerScriptService/Modules/CoinManager.lua
+++ src/ServerScriptService/Modules/CoinManager.lua
@@ -1,6 +1,16 @@
 local CoinManager = {}
+CoinManager.__index = CoinManager
+
+function CoinManager:checkCoin()
+    local coins = {
+        Coins = 500;
+    }
+    setmetatable(coins, CoinManager)
+    return coins
+end
 
 function CoinManager.new()
     
 end
+
 return CoinManager
